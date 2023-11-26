15aa949cde1cdae634089aae584c32585bf522ba
Nico Bobb <nicobobb@gmail.com>
2023-11-26 15:56:21 -0300

    Refactor TaskItems component to improve readability and add hover effects

--- src/Componentes/TaskItems.jsx
+++ src/Componentes/TaskItems.jsx
@@ -9,28 +9,36 @@ const TaskItems = ({ item, eliminar, id, completado, enterEditMode }) => {
     setCheck(!check);
   };
   return (
-    <div className="flex items-center justify-between p-4">
-      <h2 className="pl-3">{item.nombreTarea}</h2>
-      <p>{!check ? "No completada" : "Completada"}</p>
-
-      <img
-        className="h-6 w-6 cursor-pointer transition-all duration-200 ease-in"
-        src="/icon-check.svg"
-        alt="icon check"
-        onClick={() => handleCheck()}
-      />
-      <img
-        className="h-6 w-6 cursor-pointer transition-all duration-200 ease-in"
-        src="/icon-editar.svg"
-        alt="icon editar"
-        onClick={() => enterEditMode(item)}
-      />
-      <img
-        className="h-6 w-6 cursor-pointer transition-all duration-200 ease-in"
-        src="/icon-eliminar.svg"
-        alt="icon eliminar"
-        onClick={() => eliminar(id)}
-      />
+    <div className="flex items-center justify-between p-4 rounded shadow-md">
+      <div className="max-w-xs">
+        <p
+          className={`pl-3 ${
+            check ? "line-through text-green-500 line-through-thick" : ""
+          }`}
+        >
+          {item.nombreTarea}
+        </p>
+      </div>
+      <div className="flex gap-2">
+        <img
+          className="h-6 w-6 cursor-pointer transition-all duration-200 ease-in hover:scale-125"
+          src="/icon-check.svg"
+          alt="icon check"
+          onClick={() => handleCheck()}
+        />
+        <img
+          className="h-6 w-6 cursor-pointer transition-all duration-200 ease-in hover:scale-125"
+          src="/icon-editar.svg"
+          alt="icon editar"
+          onClick={() => enterEditMode(item)}
+        />
+        <img
+          className="h-6 w-6 cursor-pointer transition-all duration-200 ease-in hover:scale-125"
+          src="/icon-eliminar.svg"
+          alt="icon eliminar"
+          onClick={() => eliminar(id)}
+        />
+      </div>
     </div>
   );
 };
